db5434513a6e2ad5d01bfa8e2ff0f262fb9468c2
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-05-04 18:29:05 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) console.log(err);
+  console.log(data);
+});
