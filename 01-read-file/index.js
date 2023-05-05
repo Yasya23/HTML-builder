c59e25dad8d2470654e6009c8edb3f495e8996e0
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-05-05 20:09:14 +0300

    fix:   01-read-file back to previpus version

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 
-const stream = fs.createReadStream(filePath, { encoding: 'utf8' });
+const stream = new fs.ReadStream(filePath, { encoding: 'utf8' });
 
-stream.on('data', data => console.log(data));
-stream.on('error', err => console.error(err));
+stream.on('readable', () => {
+  const data = stream.read();
+  if(data != null) console.log(data);
+});
+
+stream.on('error', (err) => {
+  console.error(err);
+});
