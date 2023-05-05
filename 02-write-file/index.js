2f248dc7d3a647cca1b91ff7f70ef57323c2651a
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-05-05 20:05:10 +0300

    feat:  02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const filePath = path.join(__dirname, 'text.txt');
+
+let text = '';
+
+const recordToTxt = () => {
+  fs.writeFile(filePath, text, (err) => {
+    if (err) { stdout.write(err);}
+  });
+};
+
+stdout.write('Write your text:\n');
+
+stdin.on('data', (data) => {
+  const exit = data.toString().trim();
+  if (exit === 'exit') { 
+    stdout.write('Good luck!\n')
+    process.exit();
+  } 
+    text += data + '\n';
+    recordToTxt();
+});
+
+process.on('SIGINT', () => {
+  stdout.write('\nGood luck!\n')
+  process.exit();
+});
+
+
+
+
+
+
+
